a8cd3c37c8d2534111d116f9f6adb9ecdff7cb14
riedadr@karsten <riedadr@gmail.com>
2023-03-19 17:57:59 +0100

    info: seperate grids

--- src/components/InfoModal.tsx
+++ src/components/InfoModal.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 import { useDisclosure } from "@mantine/hooks";
 import { Avatar, Button, Divider, Grid, Modal, Text } from "@mantine/core";
-import { IconBrandGithub, IconBrandMantine, IconBrandMysql, IconBrandNextjs, IconBrandPlanetscale, IconBrandReact, IconBrandTabler, IconCloud, IconInfoCircle } from "@tabler/icons-react";
+import {
+    IconBrandGithub,
+    IconBrandMantine,
+    IconBrandMysql,
+    IconBrandNextjs,
+    IconBrandPlanetscale,
+    IconBrandReact,
+    IconBrandTabler,
+    IconCloud,
+    IconInfoCircle,
+} from "@tabler/icons-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -38,30 +48,26 @@ export const InfoModal = () => {
                         <T3links />
                         <Divider my="sm" />
                         <Grid>
-                            <Grid.Col span={4}>
+                            <Grid.Col span={6}>
                                 <Button
                                     component="a"
                                     href="https://github.com/riedadr/t3-ta2"
                                     target="_blank"
                                     fullWidth
                                     variant="light"
-                                    leftIcon={
-                                        <IconBrandGithub />
-                                    }
+                                    leftIcon={<IconBrandGithub />}
                                 >
                                     GitHub
                                 </Button>
                             </Grid.Col>
-                            <Grid.Col span={4}>
+                            <Grid.Col span={6}>
                                 <Button
                                     component="a"
                                     href="https://www.oracle.com/cloud/"
                                     target="_blank"
                                     fullWidth
                                     variant="light"
-                                    leftIcon={
-                                        <IconCloud />
-                                    }
+                                    leftIcon={<IconCloud />}
                                 >
                                     Oracle
                                 </Button>
@@ -85,156 +91,157 @@ export const InfoModal = () => {
 
 const T3links = () => {
     return (
-        <Grid mt={8}>
-            <Grid.Col className="flex justify-center" span={12}>
-                <Button
-                    component="a"
-                    href="https://create.t3.gg/"
-                    target="_blank"
-                    fullWidth
-                    variant="gradient"
-                    gradient={{
-                        from: "#CC66FF",
-                        to: "#6900ff",
-                        deg: 45,
-                    }}
-                    leftIcon={<Avatar src="/images/t3.svg" size={28} />}
-                >
-                    T3-Stack
-                </Button>
-            </Grid.Col>
-            <Grid.Col className="flex flex-col gap-2" span={4}>
-                <Button
-                    component="a"
-                    href="https://www.typescriptlang.org/"
-                    target="_blank"
-                    fullWidth
-                    className="bg-[#3178c6]"
-                    leftIcon={
-                        <Image
-                            src="/images/ts.svg"
-                            height={28}
-                            width={28}
-                            alt=""
-                        />
-                    }
-                >
-                    TypeScript
-                </Button>
-                <Button
-                    component="a"
-                    href="https://react.dev/"
-                    target="_blank"
-                    fullWidth
-                    variant="light"
-                    leftIcon={
-                        <IconBrandReact />
-                    }
-                >
-                    React
-                </Button>
-                <Button
-                    component="a"
-                    href="https://nextjs.org/"
-                    target="_blank"
-                    fullWidth
-                    variant="light"
-                    leftIcon={
-                        <IconBrandNextjs />
-                    }
-                >
-                    NEXT.js
-                </Button>
-            </Grid.Col>
-            <Grid.Col className="flex flex-col gap-2" span={4}>
-                <Button
-                    component="a"
-                    href="https://trpc.io/"
-                    target="_blank"
-                    fullWidth
-                    className="bg-[#398CCB]"
-                    leftIcon={
-                        <Image
-                            src="/images/trpc.svg"
-                            height={28}
-                            width={28}
-                            alt=""
-                        />
-                    }
-                >
-                    tRPC
-                </Button>
-                <Button
-                    component="a"
-                    href="https://planetscale.com/"
-                    target="_blank"
-                    fullWidth
-                    variant="light"
-                    leftIcon={
-                        <IconBrandPlanetscale />
-                    }
-                >
-                    Planetscale
-                </Button>
-                <Button
-                    component="a"
-                    href="https://www.mysql.com/"
-                    target="_blank"
-                    fullWidth
-                    variant="light"
-                    leftIcon={
-                        <IconBrandMysql />
-                    }
-                >
-                    MySQL
-                </Button>
-            </Grid.Col>
-            <Grid.Col className="flex flex-col gap-2" span={4}>
-                <Button
-                    component="a"
-                    href="https://tailwindcss.com/"
-                    target="_blank"
-                    fullWidth
-                    variant="gradient"
-                    gradient={{
-                        from: "#0f172a",
-                        to: "#193251",
-                        deg: 45,
-                    }}
-                    leftIcon={
-                        <Image
-                            src="/images/tailwindcss.svg"
-                            height={28}
-                            width={28}
-                            alt=""
-                        />
-                    }
-                >
-                    Tailwind
-                </Button>
-                <Button
-                    component="a"
-                    href="https://mantine.dev/"
-                    target="_blank"
-                    fullWidth
-                    variant="light"
-                    leftIcon={
-                        <IconBrandMantine />
-                    }
-                >
-                    Mantine
-                </Button>
-                <Button
-                    component="a"
-                    href="https://tabler-icons.io/"
-                    target="_blank"
-                    fullWidth
-                    variant="light"
-                    leftIcon={<IconBrandTabler size={28} />}
-                >
-                    tabler
-                </Button>
-            </Grid.Col>
-        </Grid>
+        <>
+            <Grid mt={8}>
+                <Grid.Col className="flex justify-center" span={12}>
+                    <Button
+                        component="a"
+                        href="https://create.t3.gg/"
+                        target="_blank"
+                        fullWidth
+                        variant="gradient"
+                        gradient={{
+                            from: "#CC66FF",
+                            to: "#6900ff",
+                            deg: 45,
+                        }}
+                        leftIcon={<Avatar src="/images/t3.svg" size={28} />}
+                    >
+                        T3-Stack
+                    </Button>
+                </Grid.Col>
+                <Grid.Col className="flex flex-col gap-2" span={4}>
+                    <Button
+                        component="a"
+                        href="https://www.typescriptlang.org/"
+                        target="_blank"
+                        fullWidth
+                        className="bg-[#3178c6]"
+                        leftIcon={
+                            <Image
+                                src="/images/ts.svg"
+                                height={28}
+                                width={28}
+                                alt=""
+                            />
+                        }
+                    >
+                        TypeScript
+                    </Button>
+                </Grid.Col>
+                <Grid.Col className="flex flex-col gap-2" span={4}>
+                    <Button
+                        component="a"
+                        href="https://trpc.io/"
+                        target="_blank"
+                        fullWidth
+                        className="bg-[#398CCB]"
+                        leftIcon={
+                            <Image
+                                src="/images/trpc.svg"
+                                height={28}
+                                width={28}
+                                alt=""
+                            />
+                        }
+                    >
+                        tRPC
+                    </Button>
+                </Grid.Col>
+                <Grid.Col className="flex flex-col gap-2" span={4}>
+                    <Button
+                        component="a"
+                        href="https://tailwindcss.com/"
+                        target="_blank"
+                        fullWidth
+                        variant="gradient"
+                        gradient={{
+                            from: "#0f172a",
+                            to: "#193251",
+                            deg: 45,
+                        }}
+                        leftIcon={
+                            <Image
+                                src="/images/tailwindcss.svg"
+                                height={28}
+                                width={28}
+                                alt=""
+                            />
+                        }
+                    >
+                        Tailwind
+                    </Button>
+                </Grid.Col>
+            </Grid>
+            <Divider my="sm" variant="dashed" />
+            <Grid>
+                <Grid.Col className="flex flex-col gap-2" span={4}>
+                    <Button
+                        component="a"
+                        href="https://react.dev/"
+                        target="_blank"
+                        fullWidth
+                        variant="light"
+                        leftIcon={<IconBrandReact />}
+                    >
+                        React
+                    </Button>
+                    <Button
+                        component="a"
+                        href="https://nextjs.org/"
+                        target="_blank"
+                        fullWidth
+                        variant="light"
+                        leftIcon={<IconBrandNextjs />}
+                    >
+                        NEXT.js
+                    </Button>
+                </Grid.Col>
+                <Grid.Col className="flex flex-col gap-2" span={4}>
+                    <Button
+                        component="a"
+                        href="https://planetscale.com/"
+                        target="_blank"
+                        fullWidth
+                        variant="light"
+                        leftIcon={<IconBrandPlanetscale />}
+                    >
+                        Planetscale
+                    </Button>
+                    <Button
+                        component="a"
+                        href="https://www.mysql.com/"
+                        target="_blank"
+                        fullWidth
+                        variant="light"
+                        leftIcon={<IconBrandMysql />}
+                    >
+                        MySQL
+                    </Button>
+                </Grid.Col>
+                <Grid.Col className="flex flex-col gap-2" span={4}>
+                    <Button
+                        component="a"
+                        href="https://mantine.dev/"
+                        target="_blank"
+                        fullWidth
+                        variant="light"
+                        leftIcon={<IconBrandMantine />}
+                    >
+                        Mantine
+                    </Button>
+                    <Button
+                        component="a"
+                        href="https://tabler-icons.io/"
+                        target="_blank"
+                        fullWidth
+                        variant="light"
+                        leftIcon={<IconBrandTabler size={28} />}
+                    >
+                        tabler
+                    </Button>
+                </Grid.Col>
+            </Grid>
+        </>
     );
 };
