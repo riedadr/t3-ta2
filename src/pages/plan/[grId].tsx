7302179a1bc49792dde86a6301977f7531ed9b73
riedadr@karsten <riedadr@gmail.com>
2023-03-19 18:10:06 +0100

    table: dynamic routes

--- src/pages/plan/[grId].tsx
+++ src/pages/plan/[grId].tsx
@@ -1,21 +1,22 @@
 import React from "react";
+import Head from "next/head";
 import { type InferGetStaticPropsType } from "next";
 import { createProxySSGHelpers } from "@trpc/react-query/ssg";
 import { dbRouter } from "~/server/api/routers/db";
 import { type TkwNrs } from "~/types/db";
 import Stundenplan from "~/components/Stundenplan";
 import { Table } from "@mantine/core";
-import Head from "next/head";
+import { useRouter } from 'next/router'
 
-const group = 41;
 
-export async function getStaticProps() {
+
+export async function getStaticProps({params}: {params: {grId: string}}) {
     const ssg = createProxySSGHelpers({
         router: dbRouter,
         ctx: {},
     });
 
-    const res = await ssg.gr.fetch({ gruppe: group });
+    const res = await ssg.gr.fetch({ gruppe: parseInt(params.grId) });
 
     return {
         props: {
@@ -26,9 +27,22 @@ export async function getStaticProps() {
     };
 }
 
-export default function gr40(
+export function getStaticPaths() {
+	return {
+		paths: [
+			"/plan/40",
+			"/plan/41",
+			"/plan/42",
+		],
+		fallback: true
+	}
+}
+
+export default function StundenplanPage(
     props: InferGetStaticPropsType<typeof getStaticProps>
 ) {
+	const router = useRouter();
+    const { grId } = router.query;
     const { data } = props;
     const wochenplan = data.result[0]?.result || {};
     const wochen = Object.keys(wochenplan as object);
@@ -36,10 +50,10 @@ export default function gr40(
     return (
         <>
             <Head>
-                <title>Stundenplan - 20/41</title>
+                <title>Stundenplan - 20/40</title>
                 <meta name="description" content="Stundenplan VI20/23 TA2" />
             </Head>
-            <h1>Gruppe {group}</h1>
+            <h1>Gruppe {grId}</h1>
             <Table striped highlightOnHover>
                 {wochen.map((key) => (
                     <Stundenplan
