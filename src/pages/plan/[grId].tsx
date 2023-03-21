8d9c35edade2cc9f8626bfdedc3bccbf5be62b47
riedadr@THEODOR <riedadr@gmail.com>
2023-03-21 11:18:09 +0100

    table: color description

--- src/pages/plan/[grId].tsx
+++ src/pages/plan/[grId].tsx
@@ -5,12 +5,10 @@ import { createProxySSGHelpers } from "@trpc/react-query/ssg";
 import { dbRouter } from "~/server/api/routers/db";
 import { type TkwNrs } from "~/types/db";
 import Stundenplan from "~/components/Stundenplan";
-import { Table } from "@mantine/core";
-import { useRouter } from 'next/router'
+import { ColorSwatch, Group, Table, Text } from "@mantine/core";
+import { useRouter } from "next/router";
 
-
-
-export async function getStaticProps({params}: {params: {grId: string}}) {
+export async function getStaticProps({ params }: { params: { grId: string } }) {
     const ssg = createProxySSGHelpers({
         router: dbRouter,
         ctx: {},
@@ -23,25 +21,21 @@ export async function getStaticProps({params}: {params: {grId: string}}) {
             trpcState: ssg.dehydrate(),
             data: res,
         },
-        revalidate: 1
+        revalidate: 1,
     };
 }
 
 export function getStaticPaths() {
-	return {
-		paths: [
-			"/plan/40",
-			"/plan/41",
-			"/plan/42",
-		],
-		fallback: false
-	}
+    return {
+        paths: ["/plan/40", "/plan/41", "/plan/42"],
+        fallback: false,
+    };
 }
 
 export default function StundenplanPage(
     props: InferGetStaticPropsType<typeof getStaticProps>
 ) {
-	const router = useRouter();
+    const router = useRouter();
     const { grId } = router.query;
     const { data } = props;
     const wochenplan = data.result[0]?.result || {};
@@ -53,7 +47,23 @@ export default function StundenplanPage(
                 <title>Stundenplan - 20/40</title>
                 <meta name="description" content="Stundenplan VI20/23 TA2" />
             </Head>
-            <h1>Gruppe {grId}</h1>
+            <div className="flex flex-wrap items-baseline justify-between gap-4">
+                <h1>Gruppe {grId}</h1>
+                <div className="flex justify-end gap-2">
+                    <div className="flex items-baseline gap-1">
+                        <ColorSwatch size={10} color="red" />
+                        <Text fz="sm">entfällt</Text>
+                    </div>
+                    <div className="flex items-baseline gap-1">
+                        <ColorSwatch size={10} color="green" />
+                        <Text fz="sm">ergänzt</Text>
+                    </div>
+                    <div className="flex items-baseline gap-1">
+                        <ColorSwatch size={10} color="yellow" />
+                        <Text fz="sm">geändert</Text>
+                    </div>
+                </div>
+            </div>
             <Table striped highlightOnHover>
                 {wochen.map((key) => (
                     <Stundenplan
